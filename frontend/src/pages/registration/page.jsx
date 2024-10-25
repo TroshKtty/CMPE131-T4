2415f602caeaaeb5651a2919a401ae58c6b475ff
Kshitij <kshitij72004@gmail.com>
2024-10-24 23:58:35 -0700

    Registration page with roles

--- frontend/src/pages/registration/page.jsx
+++ frontend/src/pages/registration/page.jsx
@@ -1,100 +1,156 @@
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Typography,
-  } from "@mui/joy";
-  import { useState } from "react";
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Typography,
+  Radio,
+  RadioGroup,
+} from "@mui/joy";
+import { useState } from "react";
 
-  export default function RegistrationPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [phone, setPhone] = useState("");
-    // const [otp, setOtp] = useState("");
-  
-    const handleRegistration = (ev) => {
-      ev.preventDefault();
-      console.log("form submitted with", {
-        email,
-        password,
-        phone
-      });
-    };
-  
-    return (
-      <>
-        <Box
-          component="nav"
-          sx={{
-            p: 2,
-            borderBottom: "1px solid",
-            borderColor: "divider",
-            position: "fixed",
-            top: 0,
-            width: "100%",
-            zIndex: 1000,
-            display: "flex",
-            justifyContent: "center",
-            bgcolor: "background.surface",
-          }}
-        >
-          <Typography level="h1" fontWeight="bold">
-            OFS
+export default function RegistrationPage() {
+  const [email, setEmail] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [role, setRole] = useState("Customer");
+  // const [otp, setOtp] = useState("");
+
+  const handleRegistration = (ev) => {
+    ev.preventDefault();
+    console.log("form submitted with", {
+      email,
+      username,
+      password,
+      phone
+    });
+  };
+
+  return (
+    <>
+      <Box
+        component="nav"
+        sx={{
+          p: 2,
+          borderBottom: "1px solid",
+          borderColor: "divider",
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          zIndex: 1000,
+          display: "flex",
+          justifyContent: "center",
+          bgcolor: "background.surface",
+        }}
+      >
+        <Typography level="h1" fontWeight="bold">
+          OFS
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box width="50%">
+          <Typography level="h2" marginBottom={2}>
+            Register
           </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100vh",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box width="50%">
-            <Typography level="h2" marginBottom={2}>
-              Register
-            </Typography>
-            <Box>
-              <form onSubmit={handleRegistration}>
-                <Box
-                  sx={{ display: "flex", flexDirection: "column", gap: "16px" }}
+          <Box>
+            <form onSubmit={handleRegistration}>
+              <Box
+                sx={{ display: "flex", flexDirection: "column", gap: "16px" }}
+              >
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    onChange={(ev) => setEmail(ev.target.value)}
+                    type="email"
+                    required
+                  />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Username</FormLabel>
+                  <Input
+                    onChange={(ev) => setUserName(ev.target.value)}
+                    type="text"
+                    required
+                  />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    type="password"
+                    required
+                  />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Phone</FormLabel>
+                  <Input
+                    onChange={(ev) => setPhone(ev.target.value)}
+                    type="text"
+                    required
+                  />
+                </FormControl>
+                <FormControl required>
+                  Role:
+                <RadioGroup
+                  orientation="horizontal"
+                  aria-labelledby="segmented-controls-example"
+                  name="role"
+                  value={role}
+                  onChange={(event) => setRole(event.target.value)}
+                  sx={{
+                    minHeight: 48,
+                    width: 310,
+                    padding: '4px',
+                    borderRadius: '12px',
+                    bgcolor: 'neutral.softBg',
+                    '--RadioGroup-gap': '4px',
+                    '--Radio-actionRadius': '8px',
+                  }}
                 >
-                  <FormControl required>
-                    <FormLabel>Email</FormLabel>
-                    <Input
-                      onChange={(ev) => setEmail(ev.target.value)}
-                      type="email"
-                      required
+                  {['Customer', 'Employee', 'Admin'].map((item) => (
+                <Radio
+                      key={item}
+                      color="primary"
+                      value={item}
+                      disableIcon
+                      label={item}
+                      variant="plain"
+                      sx={{ px: 2, alignItems: 'center' }}
+                      slotProps={{
+                        action: ({ checked }) => ({
+                          sx: {
+                            ...(checked && {
+                              bgcolor: 'background.surface',
+                              boxShadow: 'sm',
+                              '&:hover': {
+                                bgcolor: 'background.surface',
+                              },
+                            }),
+                          },
+                        }),
+                      }}
                     />
-                  </FormControl>
-                  <FormControl required>
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                      onChange={(ev) => setPassword(ev.target.value)}
-                      type="password"
-                      required
-                    />
-                  </FormControl>
-                  <FormControl required>
-                    <FormLabel>Phone</FormLabel>
-                    <Input
-                      onChange={(ev) => setPhone(ev.target.value)}
-                      type="text"
-                      required
-                    />
-                  </FormControl>
-                  <Button type="submit" fullWidth>
-                    Register
-                  </Button>
-                </Box>
-              </form>
-            </Box>
+                  ))}
+                </RadioGroup>
+                </FormControl>
+                <Button type="submit" fullWidth>
+                  Register
+                </Button>
+              </Box>
+            </form>
           </Box>
         </Box>
-      </>
-    );
-  }
-  
+      </Box>
+    </>
+  );
+}
