998b84ea4ffecdcaf8ef6c0a839d3a7f469fe55b
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-11-03 22:26:45 -0800

    Fix 'product_test' route

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_IP,
--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,33 @@
-// index.js or app.js
-const express = require('express');
-const sequelize = require('./config'); // Import sequelize instance
-const cors = require('cors');
-require('dotenv').config();
-const token = require('./middleware/auth');
-const auth_route = require('./routes/auth_routes');
+require("dotenv").config();
 
-const { Product } = require('./models');
+const express = require("express");
+const sequelize = require("./config");
+const cors = require("cors");
+const auth_route = require("./routes/auth_routes");
 
-
-const app = express();
-app.use(express.json());
-
-// const db = require('./models');
-
-
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true, // Allow credentials to be included
-}));
-
-app.use('/auth', auth_route);
+const { ProductTest } = require("./models");
 
 const PORT = 3000;
+const app = express();
 
-// Test the database connection
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connected to the database successfully.');
-    })
-    .catch((error) => {
-        console.error('Unable to connect to the database:', error);
-});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-/*
-// API endpoint to fetch products  DOESNT WORK
-app.get('/api/products', (req, res) => {
-  db.query('SELECT * FROM products', (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(results);
+app.use("/auth", auth_route);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to the database successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
   });
-});
-*/
 
 // // Start server (Line 15 commented out)
 // db.sequelize.sync().then((req) => {
@@ -52,17 +36,16 @@ app.get('/api/products', (req, res) => {
 //     });
 // });
 
-app.get('/api/product_test', async (req, res) => {
+app.get("/api/product_test", async (req, res) => {
   try {
-    const products = await Product.findAll(); // Retrieves all products
+    const products = await ProductTest.findAll();
     res.json(products);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch products' });
+    console.log("Error fetching products:", error);
+    res.status(500).json({ error: "Failed to fetch products" });
   }
 });
 
-
 app.listen(PORT, () => {
-        console.log(`Server running on Port ${PORT}`);
+  console.log(`Server running on Port ${PORT}`);
 });
-
