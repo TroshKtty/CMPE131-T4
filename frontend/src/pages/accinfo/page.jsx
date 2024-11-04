439660087c336964286acf82d09dca1f572af17b
Memekushi <3piclolz8901@gmail.com>
2024-11-04 13:23:07 -0800

    Groundwork for the account info page

---
+++ frontend/src/pages/accinfo/page.jsx
@@ -0,0 +1,18 @@
+import {
+    AspectRatio,
+    Box,
+    Button,
+    Card,
+    CardContent,
+    CardOverflow,
+    Divider,
+    Grid,
+    Link,
+    Stack,
+    Typography,
+  } from "@mui/joy";
+
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/joy";
+import { useNavigate } from 'react-router-dom';
+import "./styles.css";
