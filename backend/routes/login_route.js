43bd8a40cfcc8633a29e4d16ef14c92f6d483467
Kshitij <kshitij72004@gmail.com>
2024-11-06 00:31:12 -0800

    Why the fk are there so many extra files

--- backend/routes/login_route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const login_controller = require("../controllers/login_controller.js"); // Import the controller
-
-router.post("/", login_controller.login);
-router.get("/", (_, res) => res.json({ hello: "world" }));
-
-module.exports = router;
