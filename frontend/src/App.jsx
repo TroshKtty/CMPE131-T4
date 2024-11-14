2cd806067c85ba04a971508cdee8b4fbe6dd0cea
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-11-14 12:07:09 -0800

    fix: cart provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 import LoginPage from "@/pages/login/page";
 import RegistrationPage from "@/pages/registration/page";
 import HomePage from "@/pages/home/page";
@@ -12,65 +13,66 @@ import ProductsPage from "@/pages/products/page";
 import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/footer/Footer";
 
-// utils
 import ProtectedRoute from "./utils/protected_routes";
 import useAuth from "./utils/auth_check";
 
-// Import CheckoutPage with the correct path
+import { CartProvider } from "@/providers/CartProvider";
 import CheckoutPage from "./pages/CheckoutPage";
 
-const AuthWrapper = () => {
-  useAuth();
-  return null;
-};
+// const AuthWrapper = () => {
+//   useAuth();
+//   return null;
+// };
 
 export default function App() {
   return (
-    <Router>
-      <AuthWrapper />
-      <Routes>
-        {/* Route for logging in */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
+    <CartProvider>
+      <Router>
+        {/* <AuthWrapper /> */}
+        <Routes>
+          {/* Route for logging in */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute required_role="admin">
-              <AdminDashboardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/approval-requests"
-          element={
-            <ProtectedRoute required_role="admin">
-              <ApprovalRequestsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/*"
-          element={
-            <>
-              <NavBar />
-              <Routes>
-                {/* Root page */}
-                <Route path="/" element={<HomePage />} />
-                {/* Route for searching products */}
-                <Route path="/search" element={<ProductsPage />} />
-                {/* Route for an individual product */}
-                <Route path="/product/:product" element={<ProductPage />} />
-                {/* Route for checkout page */}
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/unauthorized" element={<UnauthorizedPage />} />
-              </Routes>
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+          {/* Protected Routes */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute required_role="admin">
+                <AdminDashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/approval-requests"
+            element={
+              <ProtectedRoute required_role="admin">
+                <ApprovalRequestsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/*"
+            element={
+              <>
+                <NavBar />
+                <Routes>
+                  {/* Root page */}
+                  <Route path="/" element={<HomePage />} />
+                  {/* Route for searching products */}
+                  <Route path="/search" element={<ProductsPage />} />
+                  {/* Route for an individual product */}
+                  <Route path="/product/:product" element={<ProductPage />} />
+                  {/* Route for checkout page */}
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/unauthorized" element={<UnauthorizedPage />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
