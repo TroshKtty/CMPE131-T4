8f8f03136812527cbf4e7be79859ac20560b6a3d
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-11-27 13:55:24 -0800

    Reduce theme roundness

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -2,10 +2,10 @@ import { extendTheme } from "@mui/joy/styles";
 
 export default extendTheme({
   radius: {
-    xs: "8px",
-    sm: "12px", // Components use default sm radius?
-    md: "16px",
-    lg: "24px",
-    xl: "32px",
+    xs: "4px", // default 2px
+    sm: "8px", // default 6px
+    md: "12px", // default 8px
+    lg: "16px", // default 12px
+    xl: "20px", // default 16px
   },
 });
