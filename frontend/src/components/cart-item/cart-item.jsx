9aa563be02970feabe60268476f97a91a6db2fed
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-11-30 21:22:39 -0800

    refactor: create CartItem component

---
+++ frontend/src/components/cart-item/cart-item.jsx
@@ -0,0 +1,77 @@
+import PropTypes from "prop-types";
+import { useCart } from "@/hooks/useCart";
+import {
+  Card,
+  Grid,
+  AspectRatio,
+  Typography,
+  Stack,
+  IconButton,
+  Button,
+} from "@mui/joy";
+import { CirclePlusIcon, CircleMinusIcon } from "lucide-react";
+
+import styles from "./cart-item.module.css";
+
+export default function CartItem({ product }) {
+  const { removeFromCart, updateQuantity } = useCart();
+
+  return (
+    <Card variant="plain" key={product.id}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid xs={4} sm={3}>
+          <AspectRatio ratio="1" objectFit="cover">
+            <img
+              src={
+                product.images.split(";")[0] ?? "https://placehold.co/400x300"
+              }
+              alt={product.name}
+              className={styles.cartItemImage}
+            />
+          </AspectRatio>
+        </Grid>
+        <Grid xs={8} sm={9}>
+          <Stack spacing={1}>
+            <Typography level="h6">{product.name}</Typography>
+            <Typography>Price: ${product.price}</Typography>
+            <div className={styles.quantityControl}>
+              <IconButton
+                size="sm"
+                onClick={() => updateQuantity(product.id, product.quantity - 1)}
+                disabled={product.quantity <= 1}
+              >
+                <CircleMinusIcon />
+              </IconButton>
+              <Typography>{product.quantity}</Typography>
+              <IconButton
+                size="sm"
+                onClick={() => updateQuantity(product.id, product.quantity + 1)}
+              >
+                <CirclePlusIcon />
+              </IconButton>
+            </div>
+            <Button
+              variant="soft"
+              color="danger"
+              onClick={() => removeFromCart(product.id)}
+              sx={{ alignSelf: "flex-start" }}
+            >
+              Remove
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
+
+CartItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    weight: PropTypes.string.isRequired,
+    images: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -1,24 +1,11 @@
+import CartItem from "@/components/cart-item/cart-item";
 import { useCart } from "@/hooks/useCart";
+import { Box, Button, Card, Grid, Stack, Typography } from "@mui/joy";
+import { ShoppingBagIcon, ShoppingBasketIcon } from "lucide-react";
 import styles from "./styles.module.css";
-import {
-  AspectRatio,
-  Button,
-  Card,
-  Grid,
-  Stack,
-  Typography,
-  IconButton,
-  Box,
-} from "@mui/joy";
-import {
-  CirclePlusIcon,
-  CircleMinusIcon,
-  ShoppingBagIcon,
-  ShoppingBasketIcon,
-} from "lucide-react";
 
 export default function CartPage() {
-  const { cart, removeFromCart, updateQuantity } = useCart();
+  const { cart } = useCart();
 
   if (!Array.isArray(cart)) {
     return (
@@ -37,16 +24,7 @@ export default function CartPage() {
 
   if (cart.length === 0) {
     return (
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "8px",
-        }}
-      >
+      <Box className={styles.emptyCartContainer}>
         <ShoppingBasketIcon size={128} />
         <Typography level="h1">Your cart is waiting for you!</Typography>
         <Typography level="title-md">
@@ -61,70 +39,14 @@ export default function CartPage() {
     <Grid container spacing={2} sx={{ p: 2 }}>
       {/* Items in cart */}
       <Grid xs={12} md={8}>
-        <Card>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 4,
-            }}
-          >
+        <Card variant="soft">
+          <div className={styles.cartHeader}>
             <ShoppingBagIcon size={24} sx={{ mt: 8 }} />
             <Typography level="h3">Shopping Cart</Typography>
           </div>
           <Stack spacing={2}>
             {cart.map((product) => (
-              <Card key={product.id}>
-                <Grid container spacing={2} alignItems="center">
-                  <Grid xs={4} sm={3}>
-                    <AspectRatio ratio="1" objectFit="cover">
-                      <img
-                        src={
-                          product.images.split(";")[0] ??
-                          "https://placehold.co/400x300"
-                        }
-                        alt={product.name}
-                        className={styles.cartItemImage}
-                      />
-                    </AspectRatio>
-                  </Grid>
-                  <Grid xs={8} sm={9}>
-                    <Stack spacing={1}>
-                      <Typography level="h6">{product.name}</Typography>
-                      <Typography>Price: ${product.price}</Typography>
-                      <div className={styles.quantityControl}>
-                        <IconButton
-                          size="sm"
-                          onClick={() =>
-                            updateQuantity(product.id, product.quantity - 1)
-                          }
-                          disabled={product.quantity <= 1}
-                        >
-                          <CircleMinusIcon />
-                        </IconButton>
-                        <Typography>{product.quantity}</Typography>
-                        <IconButton
-                          size="sm"
-                          onClick={() =>
-                            updateQuantity(product.id, product.quantity + 1)
-                          }
-                        >
-                          <CirclePlusIcon />
-                        </IconButton>
-                      </div>
-                      <Button
-                        variant="soft"
-                        color="danger"
-                        onClick={() => removeFromCart(product.id)}
-                        sx={{ alignSelf: "flex-start" }}
-                      >
-                        Remove
-                      </Button>
-                    </Stack>
-                  </Grid>
-                </Grid>
-              </Card>
+              <CartItem key={product.id} product={product} />
             ))}
           </Stack>
         </Card>
