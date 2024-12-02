7d2cdc2ca7c0039259a3901e2e706e7afb4c8444
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-12-02 15:46:10 -0800

    feat: require log in check to add items to cart

--- frontend/src/components/product-card/product-card.jsx
+++ frontend/src/components/product-card/product-card.jsx
@@ -12,10 +12,12 @@ import { Minus, Plus, ShoppingCart } from "lucide-react";
 import PropTypes from "prop-types";
 import styles from "./product-card.module.css";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function ProductCard({ product }) {
   const navigate = useNavigate();
   const { cart, addToCart, updateCount, removeFromCart } = useCart();
+  const { isLoggedIn } = useAuth();
 
   const cartItem = cart.find((item) => item.id === product.id);
   const count = cartItem ? cartItem.count : 0;
@@ -79,67 +81,71 @@ export default function ProductCard({ product }) {
           ${product.price}
         </Typography>
         <div style={{ height: "40px" }}>
-          {count === 0 ? (
-            <Button
-              fullWidth
-              variant="solid"
-              startDecorator={<ShoppingCart size={16} />}
-              onClick={handleAddToCart}
-              sx={{ mt: 1 }}
-            >
-              Add to Cart
-            </Button>
-          ) : (
-            <div className={styles.productCardInCartButtonContainer}>
-              <IconButton
-                className={styles.cartQuantityButton}
-                size="sm"
-                variant="outlined"
-                color="neutral"
-                onClick={() => handleBtnCountChange(count - 1)}
+          {isLoggedIn ? (
+            count === 0 ? (
+              <Button
+                fullWidth
+                variant="solid"
+                startDecorator={<ShoppingCart size={16} />}
+                onClick={handleAddToCart}
+                sx={{ mt: 1 }}
               >
-                <Minus size={16} />
-              </IconButton>
-              <Input
-                type="number"
-                className={styles.inputContainer}
-                size="sm"
-                value={count}
-                onChange={handleInputQuantityChange}
-                slotProps={{
-                  input: {
-                    min: 0,
-                    max: product.quantity,
-                    step: 1,
-                  },
-                }}
-                sx={{
-                  textAlign: "center",
-                  // Removes the up/down arrows for a number input
-                  "& .MuiInput-input": {
-                    textAlign: "center",
-                    "&::-webkit-inner-spin-button, &::-webkit-outer-spin-button":
-                      {
-                        "-webkit-appearance": "none",
-                        margin: 0,
-                      },
-                    "&[type=number]": {
-                      "-moz-appearance": "textfield",
+                Add to Cart
+              </Button>
+            ) : (
+              <div className={styles.productCardInCartButtonContainer}>
+                <IconButton
+                  className={styles.cartQuantityButton}
+                  size="sm"
+                  variant="outlined"
+                  color="neutral"
+                  onClick={() => handleBtnCountChange(count - 1)}
+                >
+                  <Minus size={16} />
+                </IconButton>
+                <Input
+                  type="number"
+                  className={styles.inputContainer}
+                  size="sm"
+                  value={count}
+                  onChange={handleInputQuantityChange}
+                  slotProps={{
+                    input: {
+                      min: 0,
+                      max: product.quantity,
+                      step: 1,
                     },
-                  },
-                }}
-              />
-              <IconButton
-                className={styles.cartQuantityButton}
-                size="sm"
-                variant="outlined"
-                color="neutral"
-                onClick={() => handleBtnCountChange(count + 1)}
-                disabled={count >= product.quantity}
-              >
-                <Plus size={16} />
-              </IconButton>
-            </div>
+                  }}
+                  sx={{
+                    textAlign: "center",
+                    // Removes the up/down arrows for a number input
+                    "& .MuiInput-input": {
+                      textAlign: "center",
+                      "&::-webkit-inner-spin-button, &::-webkit-outer-spin-button":
+                        {
+                          "-webkit-appearance": "none",
+                          margin: 0,
+                        },
+                      "&[type=number]": {
+                        "-moz-appearance": "textfield",
+                      },
+                    },
+                  }}
+                />
+                <IconButton
+                  className={styles.cartQuantityButton}
+                  size="sm"
+                  variant="outlined"
+                  color="neutral"
+                  onClick={() => handleBtnCountChange(count + 1)}
+                  disabled={count >= product.quantity}
+                >
+                  <Plus size={16} />
+                </IconButton>
+              </div>
+            )
+          ) : (
+            <Button variant="soft" fullWidth>Sign In To Add</Button>
           )}
         </div>
       </CardContent>
