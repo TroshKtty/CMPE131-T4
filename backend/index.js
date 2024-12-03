cbbe07776dc9ab12ef3a661a196ec34b7ec47f78
Kshitij <kshitij72004@gmail.com>
2024-12-02 19:00:06 -0800

    Some functions done, just want to merge with Tommy's changes

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,16 @@ const auth_route = require("./routes/auth_routes");
 const pending_route = require("./routes/pending_routes");
 const employeeRoutes = require('./routes/employee_routes');
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart_route");
 const Product = require("./models/product");
+const { auth, verifyPermission } = require("./middleware/auth");
+const cookieParser = require('cookie-parser');
+const { setupAssociations } = require("./models/index");
 
 const app = express();
 const PORT = 3000;
+
+setupAssociations();
 
 app.use(express.json());
 app.use(
@@ -20,10 +26,12 @@ app.use(
     credentials: true,
   })
 );
+app.use(cookieParser());
 
 app.use("/auth", auth_route);
-app.use("/users", pending_route);
+app.use("/users", auth, verifyPermission("admin"), pending_route);
 app.use("/products", productRoute);
+app.use("/cart", auth, verifyPermission("customer"), cartRoute);
 app.use('/employees', employeeRoutes);
 
 async function startServer() {
