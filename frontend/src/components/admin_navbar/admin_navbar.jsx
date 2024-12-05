3cc9ed6da5704d715ee20d7f32cccb55a05edea8
DefaultCat <66979534+DefaultCat@users.noreply.github.com>
2024-12-04 22:54:35 -0800

    added a slash

--- frontend/src/components/admin_navbar/admin_navbar.jsx
+++ frontend/src/components/admin_navbar/admin_navbar.jsx
@@ -37,7 +37,7 @@ export default function AdminSidebar() {
         </div>
       </div>
       <div className="sidebar-links">
-      <Link component={RouterLink} to="" className="sidebar-link">
+      <Link component={RouterLink} to="/" className="sidebar-link">
           Return to Dashboard
         </Link>
         <Link component={RouterLink} to="/admin/settings" className="sidebar-link">
